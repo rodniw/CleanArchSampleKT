573a53d5e9575d9fef9c66097f70ee12b4546a5f
rodniw <rodion.prosvirnin@bk.ru>
2019-09-02 18:21:32 +0300

    changed document repository. included interaction with open document data source there.

--- core/src/main/java/dev/rodni/ru/cleanarchsamplekt/data/DocumentRepository.kt
+++ core/src/main/java/dev/rodni/ru/cleanarchsamplekt/data/DocumentRepository.kt
@@ -2,11 +2,19 @@ package dev.rodni.ru.cleanarchsamplekt.data
 
 import dev.rodni.ru.cleanarchsamplekt.domain.Document
 
-class DocumentRepository(private val dataSource: DocumentDataSource) {
+class DocumentRepository(
+    private val documentDataSource: DocumentDataSource,
+    private val openDocumentDataSource: OpenDocumentDataSource
+) {
+    /* for document data source*/
+    suspend fun addDocument(document: Document) = documentDataSource.add(document)
 
-    suspend fun addDocument(document: Document) = dataSource.add(document)
+    suspend fun readAll() = documentDataSource.readAll() //return List<Document>
 
-    suspend fun readAll() = dataSource.readAll() //return List<Document>
+    suspend fun removeDocument(document: Document) = documentDataSource.remove(document)
 
-    suspend fun removeDocument(document: Document) = dataSource.remove(document)
+    /* for open document data source*/
+    fun setOpenDocument(document: Document) = openDocumentDataSource.setOpenDocument(document)
+
+    fun getOpenDocument() = openDocumentDataSource.getOpenDocument() //return Document
 }
