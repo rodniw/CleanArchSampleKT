619582c76e473cfc5e380ef941e71ccc308017e8
rodniw <rodion.prosvirnin@bk.ru>
2019-09-02 23:49:53 +0300

    written class library view model.

--- app/src/main/java/dev/rodni/ru/cleanarchsamplekt/presentation/library/LibraryViewModel.kt
+++ app/src/main/java/dev/rodni/ru/cleanarchsamplekt/presentation/library/LibraryViewModel.kt
@@ -6,22 +6,37 @@ import androidx.lifecycle.MutableLiveData
 import dev.rodni.ru.cleanarchsamplekt.domain.Document
 import dev.rodni.ru.cleanarchsamplekt.framework.Interactors
 import dev.rodni.ru.cleanarchsamplekt.framework.ReaderAppViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class LibraryViewModel(application: Application, interactors: Interactors)
   : ReaderAppViewModel(application, interactors) {
 
   val documents: MutableLiveData<List<Document>> = MutableLiveData()
 
+  //this fetches the list of documents from the library using the GetDocuments interactor
   fun loadDocuments() {
-    // TODO start loading documents
+    GlobalScope.launch {
+      documents.postValue(interactors.getDocuments())
+    }
   }
 
+  //right after adding a new document my user should be able to see this new doc inside doc's list
+  //that is why after interactors.addDocument i call loadDocuments method
+  //which loading the list of documents againg to update that list
   fun addDocument(uri: Uri) {
-    // TODO add a new document
-    loadDocuments()
+    GlobalScope.launch {
+      withContext(Dispatchers.IO) {
+        interactors.addDocument(Document(uri.toString(), "", 0, ""))
+      }
+      loadDocuments()
+    }
   }
 
   fun setOpenDocument(document: Document) {
-    // TODO set currently open document
+    interactors.setOpenDocument(document)
   }
+
 }
